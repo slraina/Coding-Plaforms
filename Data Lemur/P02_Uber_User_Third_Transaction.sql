a1e8c20f547354c7a5da64607f0116b239e1443c
Sahil Raina <72317900+slraina@users.noreply.github.com>
2022-10-02 11:18:43 +0530

    Add files via upload

---
+++ Data Lemur/P02_Uber_User_Third_Transaction.sql
@@ -0,0 +1,8 @@
+WITH atn AS (
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY transaction_date) as txns
+FROM transactions
+)SELECT user_id,	spend,	transaction_date
+FROM atn 
+WHERE txns = 3
+;
