6fda1d4abc702355cde5e22618ca299aab3d3711
Sahil Raina <72317900+slraina@users.noreply.github.com>
2022-10-02 11:16:00 +0530

    Add files via upload

---
+++ Data Lemur/P01_Linkedin_DataScienceSkills.sql
@@ -0,0 +1,10 @@
+With selectedC as 
+(
+SELECT candidate_id 
+FROM candidates
+WHERE skill IN ('Python','Tableau','PostgreSQL')
+)
+SELECT candidate_id 
+FROM selectedC
+GROUP BY candidate_id
+HAVING COUNT(candidate_id) = 3
