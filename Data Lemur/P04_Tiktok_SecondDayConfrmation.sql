a2875269ef4579bc4f97bcba1ff9e01a0ec7fd4e
Sahil Raina <72317900+slraina@users.noreply.github.com>
2022-10-02 11:23:41 +0530

    Add files via upload

---
+++ Data Lemur/P04_Tiktok_SecondDayConfrmation.sql
@@ -0,0 +1,6 @@
+SELECT e.user_id
+FROM emails e
+JOIN texts t
+  ON e.email_id = t.email_id AND 
+    (DATE_PART('day',action_date::timestamp-signup_date::timestamp)= 1 
+    AND signup_action = 'Confirmed');
